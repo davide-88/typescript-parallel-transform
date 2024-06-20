896415e12d9de35c7d5f0c2c0323020ad8e53be9
Davide Mantelli <davide.mantelli@docebo.com>
2024-06-20 12:12:33 +0200

    feat(init-project): add tsup to bundle lib

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig(options => {
+  return {
+    entry: ["src/index.ts"],
+    outDir: "lib",
+    target: "node20",
+    format: ["esm", "cjs"],
+    dts: true,
+    shims: true,
+    treeshake: true,
+    sourcemap: !options.watch,
+    minify: !options.watch,
+    clean: true,
+  };
+});
